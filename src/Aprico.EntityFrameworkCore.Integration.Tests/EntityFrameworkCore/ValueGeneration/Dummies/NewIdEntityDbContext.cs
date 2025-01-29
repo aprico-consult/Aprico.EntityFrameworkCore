326c193183db0ddaad6a99796d056961054f55ed
François Chabot <francois.chabot@stateless.be>
2025-01-29 21:40:44 +0100

    Add NodaTime Instant value converters, sequential GUID generation, and integration tests
    - Introduced `InstantToLongConverter` and `InstantToDateTimeConverter` to enable seamless NodaTime support in EF Core, ensuring compatibility with database operations.
    - Added `NewId` library for sequential GUID generation and implemented `NewIdValueGenerator` for EF Core. Adjusted project dependencies to accommodate these additions.
    - Developed and extended comprehensive integration tests for value converters and sequential GUIDs, including supporting fixtures, configurations, and entities.

---
+++ src/Aprico.EntityFrameworkCore.Integration.Tests/EntityFrameworkCore/ValueGeneration/Dummies/NewIdEntityDbContext.cs
@@ -0,0 +1,34 @@
+#region region Copyright & License
+
+// Copyright © 2024 - 2025 Aprico Consultants
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using Aprico.EntityFrameworkCore.Storage.ValueConversion.Dummies;
+using Aprico.Xunit;
+using Microsoft.Data.SqlClient;
+
+namespace Aprico.EntityFrameworkCore.ValueGeneration.Dummies;
+
+public class NewIdEntityDbContext(string connectionString) : SingleEntityDbContext<NewIdEntity, NewIdEntityConfiguration>(connectionString), ICreateDbContext<NewIdEntityDbContext>
+{
+	public static NewIdEntityDbContext Create(string connectionString)
+	{
+		var builder = new SqlConnectionStringBuilder(connectionString) {
+			InitialCatalog = nameof(NewIdEntityDbContext)
+		};
+		return new NewIdEntityDbContext(builder.ToString());
+	}
+}
